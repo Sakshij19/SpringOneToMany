e1a557a9ad4f255df428967d09c202a2f836cadb
Sakshi <sakshi.jadhav@capgemini.com>
2023-01-15 13:36:44 +0530

    SpringOneToMany

---
+++ src/main/java/com/cg/SpringOneToManyStudCourApplication.java
@@ -0,0 +1,14 @@
+package com.cg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOneToManyStudCourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOneToManyStudCourApplication.class, args);
+		System.out.println("App Started");
+	}
+
+}
